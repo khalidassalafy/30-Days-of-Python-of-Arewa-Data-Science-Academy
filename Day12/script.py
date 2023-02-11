a5b69f6d3c3bbcbf80d37a96dea2563bb18365ea
khalidassalafy <124467238+khalidassalafy@users.noreply.github.com>
2023-02-11 13:55:08 +0100

    Daily exercises continuation

---
+++ Day12/script.py
@@ -0,0 +1,7 @@
+import sys
+#print(sys.argv[0], argv[1],sys.argv[2])  # this line would print out: filename argument1 argument2
+#print('Welcome {}. Enjoy  {} challenge!'.format(sys.argv[0], sys.argv[1]))
+
+from random import randint,random
+print(randint(5, 20))
+print(random())
